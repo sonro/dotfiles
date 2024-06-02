cec122bf64ed8d3a050707cbc2818c60a603c83c
Christopher Morton <sonro@gmx.com>
2024-06-02 10:52:15 +0100

    Update comment color

--- nvim/newconfig/lua/topher/plugins/colorscheme.lua
+++ nvim/newconfig/lua/topher/plugins/colorscheme.lua
@@ -1,41 +1,45 @@
+COMMENT_COLOR = "#db4b4b"
 return {
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000, 
-        config = function()
-            require("gruvbox").setup({
-                contrast = "hard",
-            })
-            -- vim.cmd("colorscheme gruvbox")
-        end,
-    },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000, 
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha",
-            })
-            -- vim.cmd("colorscheme catppuccin")
-        end,
-    },
-    {
-        "folke/tokyonight.nvim",
-        priority = 1000,
-        config = function()
-            local transparent = false
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				contrast = "hard",
+			})
+			-- vim.cmd("colorscheme gruvbox")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+			})
+			-- vim.cmd("colorscheme catppuccin")
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		config = function()
+			local transparent = false
 
-            require("tokyonight").setup({
-                style = "night",
-                transparent = transparent,
-                styles = {
-                    sidebars = transparent and "transparent" or "dark",
-                    floats = transparent and "transparent" or "dark",
-                },
-            })
+			require("tokyonight").setup({
+				style = "night",
+				transparent = transparent,
+				styles = {
+					sidebars = transparent and "transparent" or "dark",
+					floats = transparent and "transparent" or "dark",
+				},
+				on_colors = function(colors)
+					colors.comment = colors.red1
+				end,
+			})
 
-            vim.cmd("colorscheme tokyonight")
-        end,
-    },
+			vim.cmd("colorscheme tokyonight")
+		end,
+	},
 }
