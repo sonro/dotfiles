a97e09655761437ffaa944c0b3d9a505d0be26d1
Christopher Morton <sonro@gmx.com>
2024-06-14 08:39:00 +0100

    Add nvim tab control

--- nvim/newconfig/lua/topher/plugins/bufferline.lua
+++ nvim/newconfig/lua/topher/plugins/bufferline.lua
@@ -1,11 +1,11 @@
 return {
-    "akinsho/bufferline.nvim",
-    version = "*",
-    opts = {
-        options = {
-            show_buffer_icons = false,
-            show_buffer_close_icons = false,
-            show_close_icon = false,
-        },
-    },
+	"akinsho/bufferline.nvim",
+	version = "*",
+	opts = {
+		options = {
+			show_buffer_icons = false,
+			show_buffer_close_icons = false,
+			show_close_icon = false,
+		},
+	},
 }
--- nvim/newconfig/lua/topher/plugins/todo-comments.lua
+++ nvim/newconfig/lua/topher/plugins/todo-comments.lua
@@ -1,21 +1,21 @@
 return {
-    "folke/todo-comments.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local todo_comments = require("todo-comments")
+	"folke/todo-comments.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local todo_comments = require("todo-comments")
 
-        -- set keymaps
-        local keymap = vim.keymap
+		-- set keymaps
+		local keymap = vim.keymap
 
-        keymap.set("n", "<leader>tn", function()
-            todo_comments.jump_next()
-        end, { desc = "Next todo comment" })
+		keymap.set("n", "]t", function()
+			todo_comments.jump_next()
+		end, { desc = "Next todo comment" })
 
-        keymap.set("n", "<leader>tp", function()
-            todo_comments.jump_prev()
-        end, { desc = "Previous todo comment" })
+		keymap.set("n", "[t", function()
+			todo_comments.jump_prev()
+		end, { desc = "Previous todo comment" })
 
-        todo_comments.setup()
-    end,
+		todo_comments.setup()
+	end,
 }
