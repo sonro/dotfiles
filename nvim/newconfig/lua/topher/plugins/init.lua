9598db935e24f84c1959e938ee2cc825294488b2
Christopher Morton <sonro@gmx.com>
2024-09-03 16:18:35 +0100

    Add zig formatting

--- nvim/newconfig/lua/topher/plugins/init.lua
+++ nvim/newconfig/lua/topher/plugins/init.lua
@@ -1,6 +1,8 @@
 return {
-    -- many plugins dep
-    "nvim-lua/plenary.nvim",
-    -- `-` for netrw
-    "tpope/vim-vinegar"
+	-- many plugins dep
+	"nvim-lua/plenary.nvim",
+	-- `-` for netrw
+	"tpope/vim-vinegar",
+	-- zig
+	"ziglang/zig.vim",
 }
