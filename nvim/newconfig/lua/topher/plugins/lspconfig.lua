b4eeadce9fe9179ee88d4979124485889374ffcc
Christopher Morton <sonro@gmx.com>
2025-11-13 14:15:23 +0000

    update deps

--- nvim/newconfig/lua/topher/plugins/lspconfig.lua
+++ nvim/newconfig/lua/topher/plugins/lspconfig.lua
@@ -6,6 +6,21 @@ return {
 		vim.lsp.enable("zls")
 		vim.lsp.enable("markdown_oxide")
 
+		vim.lsp.config("zls", {
+			filetypes = { "zig", "zon", "zir" },
+		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = { "*.zig", "*.zon" },
+
+			callback = function(_)
+				vim.lsp.buf.code_action({
+					context = { only = { "source.fixAll" } },
+					apply = true,
+				})
+			end,
+		})
+
 		vim.api.nvim_create_autocmd("LspAttach", {
 			callback = function(args)
 				local keymap = vim.keymap
